427c330305198bfaa18030eb642742a8f77af291
SMLALI Achraf <smlali.achraf@gmail.com>
2017-05-11 11:04:54 +0200

    fichier de donner -logs- ajoute  (close #2)

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,14 @@
 #include <iostream>
 #include <string>
-
+#include <glog/logging.h>
 int main(){
+google::InitGoogleLogging("log_chifoumi");
+ google::SetLogDestination(google::GLOG_INFO, "log_chifoumi");
  
+
   std::cout<<"hello world";
+   
+   LOG(INFO)  << "test222*******";
+
   return 0;
 }
