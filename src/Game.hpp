9d18ceb67c3aa5b786f213cc2091a72504538243
SMLALI Achraf <smlali.achraf@gmail.com>
2017-05-12 10:06:42 +0200

    documentation ajouter (close #8)

--- src/Game.hpp
+++ src/Game.hpp
@@ -1,17 +1,31 @@
 #ifndef GAME_HPP_
 #define GAME_HPP_
 #include <string>
-class Game{
-public:
-  std::string _last_result;
-  std::string _last_Ai_Move;
 
+  /**  Game Game.hpp 
+    la classe Game era utilier pour manipuler le jeu.
+    pour le premier cous il faut utiliser la methode play quui initialise les attribus avant d'utiliser les deux autre methode sui permmet de renvoyer ce attribus 
+
+
+
+ */
+class Game{
+private:
+  ///atribue qui contient le dernier choix de l'utilisateur
+  std::string _last_result;
+  ///atribue qui contient le dernier choix de l'ordi
+  std::string _last_Ai_Move;
+public:
+  ///permet d'initialiser les attribus _last_result avec le choix du user et  _last_Ai_Move avec un coix aleatoire
   void play(std::string move);
+  /// utilier pour renvoyer le dernier choix du user
   std::string get_Last_Result();
+  /// utilier pour renvoyer le dernier choix de l'ordi
   std::string get_AI_Move();
   
 };
 
+/// determine le gagnant dapres les  de choix passer en paramtre(choix IA en premier ensuite choix user )
 std::string compte_Result(std::string move_IA,std::string move_user);
 
 #endif
